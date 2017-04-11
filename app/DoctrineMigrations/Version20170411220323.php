71b347979fe3f013cb44d9d74336ec2c552428ef
Kevin Verschaeve <kevin.verschaeve@live.fr>
2017-04-12 00:04:06 +0200

    [MIGRATIONS] Install and run first migration

---
+++ app/DoctrineMigrations/Version20170411220323.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20170411220323 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'sqlite', 'Migration can only be executed safely on \'sqlite\'.');
+
+        $this->addSql('DROP INDEX UNIQ_8FB094A1E7927C74');
+        $this->addSql('DROP INDEX UNIQ_8FB094A1F85E0677');
+        $this->addSql('CREATE TEMPORARY TABLE __temp__symfony_demo_user AS SELECT id, fullName, username, email, password, roles FROM symfony_demo_user');
+        $this->addSql('DROP TABLE symfony_demo_user');
+        $this->addSql('CREATE TABLE symfony_demo_user (id INTEGER NOT NULL, fullName VARCHAR(255) NOT NULL COLLATE BINARY, username VARCHAR(255) NOT NULL COLLATE BINARY, email VARCHAR(255) NOT NULL COLLATE BINARY, password VARCHAR(255) NOT NULL COLLATE BINARY, roles CLOB NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('INSERT INTO symfony_demo_user (id, fullName, username, email, password, roles) SELECT id, fullName, username, email, password, roles FROM __temp__symfony_demo_user');
+        $this->addSql('DROP TABLE __temp__symfony_demo_user');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_8FB094A1E7927C74 ON symfony_demo_user (email)');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_8FB094A1F85E0677 ON symfony_demo_user (username)');
+        $this->addSql('DROP INDEX IDX_58A92E65F675F31B');
+        $this->addSql('CREATE TEMPORARY TABLE __temp__symfony_demo_post AS SELECT id, author_id, title, slug, summary, content, publishedAt FROM symfony_demo_post');
+        $this->addSql('DROP TABLE symfony_demo_post');
+        $this->addSql('CREATE TABLE symfony_demo_post (id INTEGER NOT NULL, author_id INTEGER NOT NULL, title VARCHAR(255) NOT NULL COLLATE BINARY, slug VARCHAR(255) NOT NULL COLLATE BINARY, summary VARCHAR(255) NOT NULL COLLATE BINARY, content CLOB NOT NULL COLLATE BINARY, publishedAt DATETIME NOT NULL, PRIMARY KEY(id), CONSTRAINT FK_58A92E65F675F31B FOREIGN KEY (author_id) REFERENCES symfony_demo_user (id) NOT DEFERRABLE INITIALLY IMMEDIATE)');
+        $this->addSql('INSERT INTO symfony_demo_post (id, author_id, title, slug, summary, content, publishedAt) SELECT id, author_id, title, slug, summary, content, publishedAt FROM __temp__symfony_demo_post');
+        $this->addSql('DROP TABLE __temp__symfony_demo_post');
+        $this->addSql('CREATE INDEX IDX_58A92E65F675F31B ON symfony_demo_post (author_id)');
+        $this->addSql('DROP INDEX IDX_6ABC1CC4BAD26311');
+        $this->addSql('DROP INDEX IDX_6ABC1CC44B89032C');
+        $this->addSql('CREATE TEMPORARY TABLE __temp__symfony_demo_post_tag AS SELECT post_id, tag_id FROM symfony_demo_post_tag');
+        $this->addSql('DROP TABLE symfony_demo_post_tag');
+        $this->addSql('CREATE TABLE symfony_demo_post_tag (post_id INTEGER NOT NULL, tag_id INTEGER NOT NULL, PRIMARY KEY(post_id, tag_id), CONSTRAINT FK_6ABC1CC44B89032C FOREIGN KEY (post_id) REFERENCES symfony_demo_post (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE, CONSTRAINT FK_6ABC1CC4BAD26311 FOREIGN KEY (tag_id) REFERENCES symfony_demo_tag (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE)');
+        $this->addSql('INSERT INTO symfony_demo_post_tag (post_id, tag_id) SELECT post_id, tag_id FROM __temp__symfony_demo_post_tag');
+        $this->addSql('DROP TABLE __temp__symfony_demo_post_tag');
+        $this->addSql('CREATE INDEX IDX_6ABC1CC4BAD26311 ON symfony_demo_post_tag (tag_id)');
+        $this->addSql('CREATE INDEX IDX_6ABC1CC44B89032C ON symfony_demo_post_tag (post_id)');
+        $this->addSql('DROP INDEX IDX_53AD8F83F675F31B');
+        $this->addSql('DROP INDEX IDX_53AD8F834B89032C');
+        $this->addSql('CREATE TEMPORARY TABLE __temp__symfony_demo_comment AS SELECT id, post_id, author_id, content, publishedAt FROM symfony_demo_comment');
+        $this->addSql('DROP TABLE symfony_demo_comment');
+        $this->addSql('CREATE TABLE symfony_demo_comment (id INTEGER NOT NULL, post_id INTEGER NOT NULL, author_id INTEGER NOT NULL, content CLOB NOT NULL COLLATE BINARY, publishedAt DATETIME NOT NULL, PRIMARY KEY(id), CONSTRAINT FK_53AD8F834B89032C FOREIGN KEY (post_id) REFERENCES symfony_demo_post (id) NOT DEFERRABLE INITIALLY IMMEDIATE, CONSTRAINT FK_53AD8F83F675F31B FOREIGN KEY (author_id) REFERENCES symfony_demo_user (id) NOT DEFERRABLE INITIALLY IMMEDIATE)');
+        $this->addSql('INSERT INTO symfony_demo_comment (id, post_id, author_id, content, publishedAt) SELECT id, post_id, author_id, content, publishedAt FROM __temp__symfony_demo_comment');
+        $this->addSql('DROP TABLE __temp__symfony_demo_comment');
+        $this->addSql('CREATE INDEX IDX_53AD8F83F675F31B ON symfony_demo_comment (author_id)');
+        $this->addSql('CREATE INDEX IDX_53AD8F834B89032C ON symfony_demo_comment (post_id)');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'sqlite', 'Migration can only be executed safely on \'sqlite\'.');
+
+        $this->addSql('DROP INDEX IDX_53AD8F834B89032C');
+        $this->addSql('DROP INDEX IDX_53AD8F83F675F31B');
+        $this->addSql('CREATE TEMPORARY TABLE __temp__symfony_demo_comment AS SELECT id, post_id, author_id, content, publishedAt FROM symfony_demo_comment');
+        $this->addSql('DROP TABLE symfony_demo_comment');
+        $this->addSql('CREATE TABLE symfony_demo_comment (id INTEGER NOT NULL, post_id INTEGER NOT NULL, author_id INTEGER NOT NULL, content CLOB NOT NULL, publishedAt DATETIME NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('INSERT INTO symfony_demo_comment (id, post_id, author_id, content, publishedAt) SELECT id, post_id, author_id, content, publishedAt FROM __temp__symfony_demo_comment');
+        $this->addSql('DROP TABLE __temp__symfony_demo_comment');
+        $this->addSql('CREATE INDEX IDX_53AD8F834B89032C ON symfony_demo_comment (post_id)');
+        $this->addSql('CREATE INDEX IDX_53AD8F83F675F31B ON symfony_demo_comment (author_id)');
+        $this->addSql('DROP INDEX IDX_58A92E65F675F31B');
+        $this->addSql('CREATE TEMPORARY TABLE __temp__symfony_demo_post AS SELECT id, author_id, title, slug, summary, content, publishedAt FROM symfony_demo_post');
+        $this->addSql('DROP TABLE symfony_demo_post');
+        $this->addSql('CREATE TABLE symfony_demo_post (id INTEGER NOT NULL, author_id INTEGER NOT NULL, title VARCHAR(255) NOT NULL, slug VARCHAR(255) NOT NULL, summary VARCHAR(255) NOT NULL, content CLOB NOT NULL, publishedAt DATETIME NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('INSERT INTO symfony_demo_post (id, author_id, title, slug, summary, content, publishedAt) SELECT id, author_id, title, slug, summary, content, publishedAt FROM __temp__symfony_demo_post');
+        $this->addSql('DROP TABLE __temp__symfony_demo_post');
+        $this->addSql('CREATE INDEX IDX_58A92E65F675F31B ON symfony_demo_post (author_id)');
+        $this->addSql('DROP INDEX IDX_6ABC1CC44B89032C');
+        $this->addSql('DROP INDEX IDX_6ABC1CC4BAD26311');
+        $this->addSql('CREATE TEMPORARY TABLE __temp__symfony_demo_post_tag AS SELECT post_id, tag_id FROM symfony_demo_post_tag');
+        $this->addSql('DROP TABLE symfony_demo_post_tag');
+        $this->addSql('CREATE TABLE symfony_demo_post_tag (post_id INTEGER NOT NULL, tag_id INTEGER NOT NULL, PRIMARY KEY(post_id, tag_id))');
+        $this->addSql('INSERT INTO symfony_demo_post_tag (post_id, tag_id) SELECT post_id, tag_id FROM __temp__symfony_demo_post_tag');
+        $this->addSql('DROP TABLE __temp__symfony_demo_post_tag');
+        $this->addSql('CREATE INDEX IDX_6ABC1CC44B89032C ON symfony_demo_post_tag (post_id)');
+        $this->addSql('CREATE INDEX IDX_6ABC1CC4BAD26311 ON symfony_demo_post_tag (tag_id)');
+        $this->addSql('DROP INDEX UNIQ_8FB094A1F85E0677');
+        $this->addSql('DROP INDEX UNIQ_8FB094A1E7927C74');
+        $this->addSql('CREATE TEMPORARY TABLE __temp__symfony_demo_user AS SELECT id, fullName, username, email, password, roles FROM symfony_demo_user');
+        $this->addSql('DROP TABLE symfony_demo_user');
+        $this->addSql('CREATE TABLE symfony_demo_user (id INTEGER NOT NULL, fullName VARCHAR(255) NOT NULL, username VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, roles CLOB NOT NULL COLLATE BINARY, PRIMARY KEY(id))');
+        $this->addSql('INSERT INTO symfony_demo_user (id, fullName, username, email, password, roles) SELECT id, fullName, username, email, password, roles FROM __temp__symfony_demo_user');
+        $this->addSql('DROP TABLE __temp__symfony_demo_user');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_8FB094A1F85E0677 ON symfony_demo_user (username)');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_8FB094A1E7927C74 ON symfony_demo_user (email)');
+    }
+}
